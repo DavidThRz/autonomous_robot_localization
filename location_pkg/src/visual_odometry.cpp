117cf83cabba224611ec6251c0a400d0933c156f
DavidThRz <davdthor@gmail.com>
2025-09-12 18:35:46 +0200

    feat: added code to capture frame from camera

--- location_pkg/src/visual_odometry.cpp
+++ location_pkg/src/visual_odometry.cpp
@@ -1,22 +1,51 @@
 
 #include "rclcpp/rclcpp.hpp"
+#include <opencv2/opencv.hpp>
+#include <boost/filesystem.hpp>
+
+
+bool captureFrame(cv::Mat& frame)
+{
+    static cv::VideoCapture cap(0, cv::CAP_V4L2 );
+
+    if (!cap.isOpened()) 
+    {
+        std::cerr << "Camera could not be opened" << std::endl;
+        return false;
+    }
+
+    cap >> frame;
+
+    if (frame.empty()) 
+    {
+        std::cerr << "Failed to capture frame" << std::endl;
+        return false;
+    }
+
+    cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+
+    //TODO: Undistort image
+
+    return true;
+}
 
 
 int main(int argc, char **argv) 
 {
-  rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);
 
-  auto node = rclcpp::Node::make_shared("nodo_loop_manual");
-  rclcpp::Rate rate(10);
+    auto node = rclcpp::Node::make_shared("visual_node");
+    rclcpp::Rate rate(1);
 
-  while (rclcpp::ok()) 
-  {
-    RCLCPP_INFO(node->get_logger(), "Iteración del loop");
+    cv::Mat new_frame;
 
-    // Equivale a spinOnce
-    rclcpp::spin_some(node);
-    rate.sleep();
-  }
+    while (rclcpp::ok()) 
+    {
+        captureFrame(new_frame);
 
-  rclcpp::shutdown();
+        rclcpp::spin_some(node);
+        rate.sleep();
+    }
+
+    rclcpp::shutdown();
 }
