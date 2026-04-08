2411b4f147be308e41016cdd2805f1947354d17e
David Thomas <david.thomas.ruiz@alumnos.upm.es>
2026-04-08 20:05:00 +0200

    feat: added covariances calculation and publication

--- include/autonomous_robot_localization/imu_class.hpp
+++ include/autonomous_robot_localization/imu_class.hpp
@@ -12,7 +12,8 @@
 enum class ImuState
 {
     RUNNING,
-    CALIBRATING
+    CALIBRATING,
+    CALIBRATING_COVARIANCES
 };
 
 class IMU_Node : public rclcpp::Node
@@ -25,9 +26,17 @@ public:
 
     void calibrateIMU(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::shared_ptr<std_srvs::srv::Trigger::Response> response);
 
+    void calibrateIMUCovariances(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::shared_ptr<std_srvs::srv::Trigger::Response> response);
+
 private:
+
+    void computeCalibration(const sensor_msgs::msg::Imu& imu_msg);
+
+    void computeCovariancesCalibration(const sensor_msgs::msg::Imu& imu_msg);
+
     rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_publisher;
     rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr calibration_service;
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr covariance_calibration_service;
     rclcpp::TimerBase::SharedPtr timer;
 
     ADIS16460_driver* imu_driver_;
@@ -35,8 +44,15 @@ private:
 
     double sum_gyro_x, sum_gyro_y, sum_gyro_z;
     double sum_accl_x, sum_accl_y, sum_accl_z;
+
+    std::vector<double> gyro_x_samples, gyro_y_samples, gyro_z_samples;
+    std::vector<double> accl_x_samples, accl_y_samples, accl_z_samples;
+
     uint16_t num_samples_;
     rclcpp::Time calibration_start_time_;
+
+    double gyro_x_covariance_, gyro_y_covariance_, gyro_z_covariance_;
+    double accl_x_covariance_, accl_y_covariance_, accl_z_covariance_;
 };
 
 #endif
--- src/imu_class.cpp
+++ src/imu_class.cpp
@@ -32,13 +32,22 @@ IMU_Node::IMU_Node() : Node("imu_node"), imu_driver_(nullptr)
 
     calibration_service = 
         this->create_service<std_srvs::srv::Trigger>(
-        "calibrate_imu",
+        "imu_calibration",
         std::bind(&IMU_Node::calibrateIMU, this, std::placeholders::_1, std::placeholders::_2)
+    );
+
+    covariance_calibration_service = 
+        this->create_service<std_srvs::srv::Trigger>(
+        "imu_covariance_calibration",
+        std::bind(&IMU_Node::calibrateIMUCovariances, this, std::placeholders::_1, std::placeholders::_2)
     );
 
     sum_gyro_x = sum_gyro_y = sum_gyro_z = 0.0;
     sum_accl_x = sum_accl_y = sum_accl_z = 0.0;
     num_samples_ = 0;
+
+    gyro_x_covariance_ = gyro_y_covariance_ = gyro_z_covariance_ = 1.0e-6;
+    accl_x_covariance_ = accl_y_covariance_ = accl_z_covariance_ = 1.0e-3;
 
     imu_state_ = ImuState::RUNNING;
 
@@ -71,36 +80,21 @@ void IMU_Node::publishIMUData()
     imu_msg.linear_acceleration.x = accl_x;
     imu_msg.linear_acceleration.y = accl_y;
     imu_msg.linear_acceleration.z = accl_z;
+    imu_msg.angular_velocity_covariance[0] = gyro_x_covariance_;
+    imu_msg.angular_velocity_covariance[4] = gyro_y_covariance_;
+    imu_msg.angular_velocity_covariance[8] = gyro_z_covariance_;
+    imu_msg.linear_acceleration_covariance[0] = accl_x_covariance_;
+    imu_msg.linear_acceleration_covariance[4] = accl_y_covariance_;
+    imu_msg.linear_acceleration_covariance[8] = accl_z_covariance_;
     imu_publisher->publish(imu_msg);
 
     if (imu_state_ == ImuState::CALIBRATING)
     {
-        num_samples_++;
-        sum_gyro_x += gyro_x;
-        sum_gyro_y += gyro_y;
-        sum_gyro_z += gyro_z;
-        sum_accl_x += accl_x;
-        sum_accl_y += accl_y;
-        sum_accl_z += accl_z;
-
-        if ((this->get_clock()->now() - calibration_start_time_).seconds() >= CALIBRATION_DURATION_SEC)
-        {
-            sum_gyro_x /= num_samples_;
-            sum_gyro_y /= num_samples_;
-            sum_gyro_z /= num_samples_;
-            sum_accl_x /= num_samples_;
-            sum_accl_y /= num_samples_;
-            sum_accl_z /= num_samples_;
-            sum_accl_z += C_g; /* Remove gravity from Z axis */
-
-            std::cout << " >> Calibration results (bias estimates):" << std::endl;
-            std::cout << " >> Gyro bias (º/s): x=" << sum_gyro_x << ", y=" << sum_gyro_y << ", z=" << sum_gyro_z << std::endl;
-            std::cout << " >> Accel bias (mg): x=" << sum_accl_x << ", y=" << sum_accl_y << ", z=" << sum_accl_z << std::endl;
-
-            imu_driver_->setBiasOffsets(-sum_gyro_x, -sum_gyro_y, -sum_gyro_z, -sum_accl_x, -sum_accl_y, -sum_accl_z);
-
-            std::cout << " >> Calibration completed" << std::endl;
-        }
+        this->computeCalibration(imu_msg);
+    }
+    else if (imu_state_ == ImuState::CALIBRATING_COVARIANCES)
+    {
+        this->computeCovariancesCalibration(imu_msg);
     }
 }
 
@@ -109,7 +103,7 @@ void IMU_Node::calibrateIMU(const std::shared_ptr<std_srvs::srv::Trigger::Reques
     (void)request;
     std::cout << "Starting IMU calibration..." << std::endl;
 
-    if (imu_state_ == ImuState::CALIBRATING) 
+    if (imu_state_ == ImuState::CALIBRATING || imu_state_ == ImuState::CALIBRATING_COVARIANCES) 
     {
         response->success = false;
         response->message = "Calibration already in progress";
@@ -128,6 +122,130 @@ void IMU_Node::calibrateIMU(const std::shared_ptr<std_srvs::srv::Trigger::Reques
     num_samples_ = 0;
     calibration_start_time_ = this->get_clock()->now();
 
+    imu_driver_->resetBiasOffsets();
+
     response->success = true;
     response->message = "Starting calibration process";
+}
+
+void IMU_Node::calibrateIMUCovariances(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::shared_ptr<std_srvs::srv::Trigger::Response> response)
+{
+    (void)request;
+    std::cout << "Starting IMU covariance calibration..." << std::endl;
+
+    if (imu_state_ == ImuState::CALIBRATING || imu_state_ == ImuState::CALIBRATING_COVARIANCES) 
+    {
+        response->success = false;
+        response->message = "Calibration already in progress";
+        return;
+    }
+
+    imu_state_ = ImuState::CALIBRATING_COVARIANCES;
+
+    gyro_x_samples.clear();
+    gyro_y_samples.clear();
+    gyro_z_samples.clear();
+    accl_x_samples.clear();
+    accl_y_samples.clear();
+    accl_z_samples.clear();
+
+    num_samples_ = 0;
+    calibration_start_time_ = this->get_clock()->now();
+
+    response->success = true;
+    response->message = "Starting covariance calibration process";
+}
+
+void IMU_Node::computeCalibration(const sensor_msgs::msg::Imu& imu_msg)
+{
+    num_samples_++;
+    sum_gyro_x += imu_msg.angular_velocity.x;
+    sum_gyro_y += imu_msg.angular_velocity.y;
+    sum_gyro_z += imu_msg.angular_velocity.z;
+    sum_accl_x += imu_msg.linear_acceleration.x;
+    sum_accl_y += imu_msg.linear_acceleration.y;
+    sum_accl_z += imu_msg.linear_acceleration.z;
+
+    if ((this->get_clock()->now() - calibration_start_time_).seconds() < CALIBRATION_DURATION_SEC)
+        return;
+
+    sum_gyro_x /= num_samples_;
+    sum_gyro_y /= num_samples_;
+    sum_gyro_z /= num_samples_;
+    sum_accl_x /= num_samples_;
+    sum_accl_y /= num_samples_;
+    sum_accl_z /= num_samples_;
+    sum_accl_z += C_g; /* Remove gravity from Z axis */
+
+    std::cout << " >> Calibration results (bias estimates):" << std::endl;
+    std::cout << " >> Gyro bias (º/s): x=" << sum_gyro_x << ", y=" << sum_gyro_y << ", z=" << sum_gyro_z << std::endl;
+    std::cout << " >> Accel bias (mg): x=" << sum_accl_x << ", y=" << sum_accl_y << ", z=" << sum_accl_z << std::endl;
+
+    imu_driver_->setBiasOffsets(-sum_gyro_x, -sum_gyro_y, -sum_gyro_z, -sum_accl_x, -sum_accl_y, -sum_accl_z);
+
+    std::cout << " >> Calibration completed" << std::endl;
+    imu_state_ = ImuState::RUNNING;
+
+}
+
+void IMU_Node::computeCovariancesCalibration(const sensor_msgs::msg::Imu& imu_msg)
+{
+    num_samples_++;
+
+    gyro_x_samples.push_back(imu_msg.angular_velocity.x);
+    gyro_y_samples.push_back(imu_msg.angular_velocity.y);
+    gyro_z_samples.push_back(imu_msg.angular_velocity.z);
+    accl_x_samples.push_back(imu_msg.linear_acceleration.x);
+    accl_y_samples.push_back(imu_msg.linear_acceleration.y);
+    accl_z_samples.push_back(imu_msg.linear_acceleration.z);
+
+    if ((this->get_clock()->now() - calibration_start_time_).seconds() < CALIBRATION_DURATION_SEC)
+        return;
+
+    double gyro_x_var = 0.0, gyro_y_var = 0.0, gyro_z_var = 0.0;
+    double accl_x_var = 0.0, accl_y_var = 0.0, accl_z_var = 0.0;
+
+    sum_gyro_x = std::accumulate(gyro_x_samples.begin(), gyro_x_samples.end(), 0.0);
+    sum_gyro_y = std::accumulate(gyro_y_samples.begin(), gyro_y_samples.end(), 0.0);
+    sum_gyro_z = std::accumulate(gyro_z_samples.begin(), gyro_z_samples.end(), 0.0);
+    sum_accl_x = std::accumulate(accl_x_samples.begin(), accl_x_samples.end(), 0.0);
+    sum_accl_y = std::accumulate(accl_y_samples.begin(), accl_y_samples.end(), 0.0);
+    sum_accl_z = std::accumulate(accl_z_samples.begin(), accl_z_samples.end(), 0.0);
+    double mean_gyro_x = sum_gyro_x / num_samples_;
+    double mean_gyro_y = sum_gyro_y / num_samples_;
+    double mean_gyro_z = sum_gyro_z / num_samples_;
+    double mean_accl_x = sum_accl_x / num_samples_;
+    double mean_accl_y = sum_accl_y / num_samples_;
+    double mean_accl_z = sum_accl_z / num_samples_;
+
+    for (size_t i = 0; i < gyro_x_samples.size(); ++i) 
+    {
+        gyro_x_var += std::pow(gyro_x_samples[i] - mean_gyro_x, 2);
+        gyro_y_var += std::pow(gyro_y_samples[i] - mean_gyro_y, 2);
+        gyro_z_var += std::pow(gyro_z_samples[i] - mean_gyro_z, 2);
+        accl_x_var += std::pow(accl_x_samples[i] - mean_accl_x, 2);
+        accl_y_var += std::pow(accl_y_samples[i] - mean_accl_y, 2);
+        accl_z_var += std::pow(accl_z_samples[i] - mean_accl_z, 2);
+    }
+
+    gyro_x_var /= (num_samples_ - 1);
+    gyro_y_var /= (num_samples_ - 1);
+    gyro_z_var /= (num_samples_ - 1);
+    accl_x_var /= (num_samples_ - 1);
+    accl_y_var /= (num_samples_ - 1);
+    accl_z_var /= (num_samples_ - 1);
+
+    std::cout << " >> Covariance calibration results (variance estimates):" << std::endl;
+    std::cout << " >> Gyro variance (º/s)^2: x=" << gyro_x_var << ", y=" << gyro_y_var << ", z=" << gyro_z_var << std::endl;
+    std::cout << " >> Accel variance (mg)^2: x=" << accl_x_var << ", y=" << accl_y_var << ", z=" << accl_z_var << std::endl;
+
+    gyro_x_covariance_ = gyro_x_var;
+    gyro_y_covariance_ = gyro_y_var;
+    gyro_z_covariance_ = gyro_z_var;
+    accl_x_covariance_ = accl_x_var;
+    accl_y_covariance_ = accl_y_var;
+    accl_z_covariance_ = accl_z_var;
+
+    std::cout << " >> Covariance calibration completed" << std::endl;
+    imu_state_ = ImuState::RUNNING;
 }
